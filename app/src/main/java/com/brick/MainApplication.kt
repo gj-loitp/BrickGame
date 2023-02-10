91db57cb917d071435b901840997f29cfcf35657
loitp <loitp@galaxy.one>
2023-02-10 22:20:06 +0700

    up

--- app/src/main/java/com/brick/MainApplication.kt
+++ app/src/main/java/com/brick/MainApplication.kt
@@ -1,24 +1,25 @@
-package com.brick;
+package com.brick
 
-import android.app.Application;
-import android.app.NotificationManager;
-import android.os.Build;
+import android.app.Application
+import android.app.NotificationManager
+import android.os.Build
+import com.brick.Values.CHANNEL_NAME
+import com.brick.Values.SCORE_CHANNEL
+import com.brick.utils.NotificationUtil
 
-import com.brick.utils.NotificationUtil;
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
 
-import static android.app.NotificationManager.IMPORTANCE_DEFAULT;
-import static com.brick.Values.CHANNEL_NAME;
-import static com.brick.Values.SCORE_CHANNEL;
-
-public class BrickGameApp extends Application {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            final NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            val nm = getSystemService(NOTIFICATION_SERVICE) as? NotificationManager?
             if (nm != null) {
-                NotificationUtil.createChannel(nm, SCORE_CHANNEL, CHANNEL_NAME, IMPORTANCE_DEFAULT);
+                NotificationUtil.createChannel(
+                    /* nm = */ nm,
+                    /* id = */ SCORE_CHANNEL,
+                    /* channelName = */ CHANNEL_NAME,
+                    /* importance = */ NotificationManager.IMPORTANCE_DEFAULT
+                )
             }
         }
     }
