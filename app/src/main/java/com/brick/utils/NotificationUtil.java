4d10ba4b8b7b71e6ce54e9e4f6e56ed29b507eb3
loitp <loitp@galaxy.one>
2023-02-10 22:26:01 +0700

    up

--- app/src/main/java/com/brick/utils/NotificationUtil.java
+++ app/src/main/java/com/brick/utils/NotificationUtil.java
@@ -1,13 +1,16 @@
 package com.brick.utils;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -54,6 +57,16 @@ public class NotificationUtil {
 
     public void createNotification() {
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         notificationManager.notify(NOTIFICATION_ID, getNotificationBuilder());
     }
 
