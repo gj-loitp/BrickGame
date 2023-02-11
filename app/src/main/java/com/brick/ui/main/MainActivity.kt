9e138d45302ceac0b3285c0c7dae34627f05961d
loitp <loitp@galaxy.one>
2023-02-11 20:41:32 +0700

    up

--- app/src/main/java/com/brick/ui/main/MainActivity.kt
+++ app/src/main/java/com/brick/ui/main/MainActivity.kt
@@ -2,104 +2,82 @@ package com.brick.ui.main
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import butterknife.BindView
-import butterknife.ButterKnife
-import butterknife.OnClick
 import com.brick.R
 import com.brick.Values
 import com.brick.databinding.ActivityMainBinding
 import com.brick.ui.main.listeners.OnTimerStateChangedListener
-import com.brick.ui.main.views.PlayingAreaView
-import com.brick.ui.main.views.PreviewAreaView
-import com.brick.ui.main.views.ScoreView
 import com.brick.utils.DebouncedOnClickListener
 
 class MainActivity : AppCompatActivity(), OnTimerStateChangedListener {
-    @JvmField
-    @BindView(R.id.playingArea)
-    var playingAreaView: PlayingAreaView? = null
-
-    @JvmField
-    @BindView(R.id.tvScore)
-    var scoreView: ScoreView? = null
-
-    @JvmField
-    @BindView(R.id.tvNextFigure)
-    var previewAreaView: PreviewAreaView? = null
-
-    @JvmField
-    @BindView(R.id.ivPausePlay)
-    var playPauseImage: ImageView? = null
-
-    @JvmField
-    @BindView(R.id.ivRotate)
-    var rotateImage: ImageView? = null
-
-    @JvmField
-    @BindView(R.id.ivMoveDown)
-    var moveDownImage: ImageView? = null
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        ButterKnife.bind(this)
-        playingAreaView!!.setDependencies(scoreView, previewAreaView, this)
-        playingAreaView!!.cleanup()
-        playingAreaView!!.createFigureWithDelay()
-        val rotate = findViewById<ImageView>(R.id.ivRotate)
-        rotate.setOnClickListener(object :
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playingArea.setDependencies(
+            scoreView = binding.tvScore,
+            previewAreaView = binding.tvNextFigure,
+            onTimerStateChangedListener = this
+        )
+        binding.playingArea.cleanup()
+        binding.playingArea.createFigureWithDelay()
+        binding.ivRotate.setOnClickListener(object :
             DebouncedOnClickListener(Values.DEBOUNCE_DELAY_IN_MILLIS) {
             override fun onDebouncedClick(v: View?) {
-                playingAreaView!!.rotate()
+                binding.playingArea.rotate()
             }
         })
+        binding.ivMoveDown.setOnClickListener {
+            moveDown()
+        }
+        binding.ivPausePlay.setOnClickListener {
+            pausePlay()
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        if (playingAreaView!!.isTimerRunning()) {
-            playingAreaView!!.startTimer()
+        if (binding.playingArea.isTimerRunning()) {
+            binding.playingArea.startTimer()
             setControlsEnabled(true)
         }
     }
 
     override fun onStop() {
-        playingAreaView!!.cancelTimer()
+        binding.playingArea.cancelTimer()
         super.onStop()
     }
 
     override fun onDestroy() {
-        playingAreaView!!.cleanup()
+        binding.playingArea.cleanup()
         super.onDestroy()
     }
 
     private fun setControlsEnabled(isRunning: Boolean) {
-        rotateImage!!.isEnabled = isRunning
-        moveDownImage!!.isEnabled = isRunning
+        binding.ivRotate.isEnabled = isRunning
+        binding.ivMoveDown.isEnabled = isRunning
     }
 
-    @OnClick(R.id.ivMoveDown)
     fun moveDown() {
-        playingAreaView!!.fastMoveDown()
+        binding.playingArea.fastMoveDown()
     }
 
-    @OnClick(R.id.ivPausePlay)
     fun pausePlay() {
-        playingAreaView!!.handleTimerState()
+        binding.playingArea.handleTimerState()
     }
 
     override fun isTimerRunning(isRunning: Boolean) {
-        playPauseImage!!.setImageResource(if (isRunning) R.drawable.ic_pause else R.drawable.ic_resume)
+        binding.ivPausePlay.setImageResource(if (isRunning) R.drawable.ic_pause else R.drawable.ic_resume)
         setControlsEnabled(isRunning)
     }
 
     override fun disableAllControls() {
-        playPauseImage!!.isEnabled = false
+        binding.ivPausePlay.isEnabled = false
         setControlsEnabled(false)
     }
 }
