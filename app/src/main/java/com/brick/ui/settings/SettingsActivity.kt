9c16698122e1b4a2221d3ae64ed4fbbf9be077ef
loitp <loitp@galaxy.one>
2023-02-12 16:47:20 +0700

    up

--- app/src/main/java/com/brick/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/brick/ui/settings/SettingsActivity.kt
@@ -1,202 +1,205 @@
-package com.brick.ui.settings;
+package com.brick.ui.settings
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.content.ActivityNotFoundException
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import com.brick.R
+import com.brick.data.Pref
+import com.brick.databinding.ActivitySettingsBinding
+import com.brick.utils.Utils
+import com.brick.utils.Utils.getViewIdByColor
+import com.shawnlin.numberpicker.NumberPicker
 
-import com.brick.R;
-import com.brick.Values;
-import com.brick.data.Pref;
-import com.brick.utils.Utils;
-import com.shawnlin.numberpicker.NumberPicker;
+class SettingsActivity : AppCompatActivity(), SettingsView {
+    private var speedItems = ArrayList<TextView>()
 
-import java.util.List;
+    private var settingsPresenter: SettingsPresenter? = null
+    private lateinit var binding: ActivitySettingsBinding
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
-import androidx.core.graphics.drawable.DrawableCompat;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-import org.jetbrains.annotations.NotNull;
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-import butterknife.BindView;
-import butterknife.BindViews;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+        settingsPresenter = SettingsPresenter(
+            settingsView = this,
+            pref = Pref(applicationContext)
+        )
 
-public class SettingsActivity extends AppCompatActivity implements SettingsView {
+        speedItems.add(binding.tvVeryFast)
+        speedItems.add(binding.tvFast)
+        speedItems.add(binding.tvDefault)
+        speedItems.add(binding.tvSlow)
+        speedItems.add(binding.tvVerySlow)
 
-    @BindViews({R.id.tvVeryFast, R.id.tvFast, R.id.tvDefault, R.id.tvSlow, R.id.tvVerySlow})
-    List<TextView> speedItems;
-
-    @BindView(R.id.clColorPicker)
-    ConstraintLayout colorPicker;
-
-    @BindView(R.id.squaresCountNumberPicker)
-    NumberPicker squaresNumberPicker;
-
-    @BindView(R.id.sEnableHints)
-    SwitchCompat enableHintsSwitch;
-
-    private SettingsPresenter settingsPresenter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        ButterKnife.bind(this);
-        settingsPresenter = new SettingsPresenter(this,
-                new Pref(getApplicationContext()));
-        squaresNumberPicker.setOnValueChangedListener((picker, oldVal, newVal) -> settingsPresenter.setSquareCountInRow(newVal));
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (settingsPresenter != null) settingsPresenter.setValues();
-    }
-
-    @Override
-    public void markChosenColor(int oldColor, int newItemId) {
-        ImageView oldImageView = findViewById(Utils.getViewIdByColor(oldColor));
-        if (oldImageView != null) {
-            oldImageView.setImageDrawable(null);
+        binding.squaresCountNumberPicker.setOnValueChangedListener { _: NumberPicker?, _: Int, newVal: Int ->
+            settingsPresenter?.setSquareCountInRow(newVal)
         }
-        ImageView newImageView = findViewById(newItemId);
+        binding.flMoreApps.setOnClickListener {
+            showMoreApps()
+        }
+        binding.flPrivacyPolicy.setOnClickListener {
+            openPrivacyPolicy()
+        }
+        binding.sEnableHints.setOnClickListener {
+            enableHints()
+        }
+        binding.vLFigureColor.setOnClickListener {
+            chooseColorFirst()
+        }
+        binding.vSquareFigureColor.setOnClickListener {
+            chooseColorSecond()
+        }
+        binding.vLongFigureColor.setOnClickListener {
+            chooseColorThird()
+        }
+        binding.vZFigureColor.setOnClickListener {
+            chooseColorFourth()
+        }
+        binding.vTFigureColor.setOnClickListener {
+            chooseColorFifth()
+        }
+        binding.vJFigureColor.setOnClickListener {
+            chooseColorSixth()
+        }
+        binding.tvVerySlow.setOnClickListener {
+            chooseVerySlowSpeed()
+        }
+        binding.tvSlow.setOnClickListener {
+            chooseSlowSpeed()
+        }
+        binding.tvDefault.setOnClickListener {
+            chooseDefaultSpeed()
+        }
+        binding.tvFast.setOnClickListener {
+            chooseFastSpeed()
+        }
+        binding.tvVeryFast.setOnClickListener {
+            chooseVeryFastSpeed()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        settingsPresenter?.setValues()
+    }
+
+    override fun markChosenColor(
+        oldColor: Int,
+        newItemId: Int
+    ) {
+        val oldImageView = findViewById<ImageView>(getViewIdByColor(oldColor))
+        oldImageView?.setImageDrawable(null)
+        var newImageView = findViewById<ImageView>(newItemId)
         if (newImageView == null) {
-            newImageView = findViewById(R.id.vZFigureColor);
+            newImageView = findViewById(R.id.vZFigureColor)
         }
-        newImageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_ok));
+        newImageView?.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_ok))
     }
 
-    @Override
-    public void setSpeedTitle(int newItemId) {
-        for (TextView item : speedItems) {
-            final Drawable wrappedDrawable = getDrawable(item, R.color.white);
-            item.setBackground(wrappedDrawable);
-            item.setTextColor(getResources().getColor(R.color.colorPrimary));
+    override fun setSpeedTitle(newItemId: Int) {
+        for (item in speedItems) {
+            val wrappedDrawable = getDrawable(item, R.color.white)
+            item.background = wrappedDrawable
+            item.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary))
         }
-        TextView newItem = findViewById(newItemId);
+        val newItem = findViewById<TextView>(newItemId)
         if (newItem != null) {
-            final Drawable wrappedDrawable = getDrawable(newItem, R.color.colorPrimary);
-            newItem.setBackground(wrappedDrawable);
-            newItem.setTextColor(getResources().getColor(R.color.white));
+            val wrappedDrawable = getDrawable(newItem, R.color.colorPrimary)
+            newItem.background = wrappedDrawable
+            newItem.setTextColor(ContextCompat.getColor(this, R.color.white))
         }
     }
 
-    @NotNull
-    private Drawable getDrawable(TextView item, int colorId) {
-        final Drawable wrappedDrawable = DrawableCompat.wrap(item.getBackground());
-        DrawableCompat.setTintList(wrappedDrawable, ColorStateList.valueOf(getResources().getColor(colorId)));
-        return wrappedDrawable;
+    private fun getDrawable(
+        item: TextView,
+        colorId: Int
+    ): Drawable {
+        val wrappedDrawable = DrawableCompat.wrap(item.background)
+        DrawableCompat.setTintList(
+            /* drawable = */ wrappedDrawable,
+            /* tint = */ ColorStateList.valueOf(ContextCompat.getColor(this, colorId))
+        )
+        return wrappedDrawable
     }
 
-    @Override
-    public void setVerticalHintsChecked(boolean hintsEnabled) {
-        enableHintsSwitch.setChecked(hintsEnabled);
+    override fun setVerticalHintsChecked(hintsEnabled: Boolean) {
+        binding.sEnableHints.isChecked = hintsEnabled
     }
 
-    @Override
-    public void setSquaresCountInRow(int squaresCountInRow) {
-        if (squaresNumberPicker != null) squaresNumberPicker.setValue(squaresCountInRow);
+    override fun setSquaresCountInRow(squaresCountInRow: Int) {
+        binding.squaresCountNumberPicker.value = squaresCountInRow
     }
 
-    @OnClick(R.id.flMoreApps)
-    void showMoreApps() {
+    private fun showMoreApps() {
         try {
-            startActivity(Utils.showMoreApps());
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(this, getResources().getString(R.string.cannot_open_market_error_text), Toast.LENGTH_LONG).show();
+            startActivity(Utils.showMoreApps())
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                this,
+                resources.getString(R.string.cannot_open_market_error_text),
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
-    @OnClick(R.id.flPrivacyPolicy)
-    void openPrivacyPolicy() {
+    private fun openPrivacyPolicy() {
         //TODO privary
     }
 
-    @OnClick(R.id.sEnableHints)
-    void enableHints() {
-        settingsPresenter.getEvent(R.id.sEnableHints);
+    private fun enableHints() {
+        settingsPresenter?.getEvent(R.id.sEnableHints)
     }
 
-    @OnClick(R.id.flFeedback)
-    void sendFeedback() {
-        try {
-            startActivity(Utils.openGmail(this, Values.RECIPIENTS, getResources().getString(R.string.app_name)));
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(this, getResources().getString(R.string.cannot_send_email_error_text), Toast.LENGTH_LONG).show();
-        }
+    private fun chooseColorFirst() {
+        settingsPresenter?.getEvent(R.id.vLFigureColor)
     }
 
-    @OnClick(R.id.flRate)
-    void rateApp() {
-        try {
-            startActivity(Utils.openMarket(this));
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(this, getResources().getString(R.string.cannot_open_market_error_text), Toast.LENGTH_LONG).show();
-        }
+    private fun chooseColorSecond() {
+        settingsPresenter?.getEvent(R.id.vSquareFigureColor)
     }
 
-    @OnClick(R.id.vLFigureColor)
-    void chooseColorFirst() {
-        settingsPresenter.getEvent(R.id.vLFigureColor);
+    private fun chooseColorThird() {
+        settingsPresenter?.getEvent(R.id.vLongFigureColor)
     }
 
-    @OnClick(R.id.vSquareFigureColor)
-    void chooseColorSecond() {
-        settingsPresenter.getEvent(R.id.vSquareFigureColor);
+    private fun chooseColorFourth() {
+        settingsPresenter?.getEvent(R.id.vZFigureColor)
     }
 
-    @OnClick(R.id.vLongFigureColor)
-    void chooseColorThird() {
-        settingsPresenter.getEvent(R.id.vLongFigureColor);
+    private fun chooseColorFifth() {
+        settingsPresenter?.getEvent(R.id.vTFigureColor)
     }
 
-    @OnClick(R.id.vZFigureColor)
-    void chooseColorFourth() {
-        settingsPresenter.getEvent(R.id.vZFigureColor);
+    private fun chooseColorSixth() {
+        settingsPresenter?.getEvent(R.id.vJFigureColor)
     }
 
-    @OnClick(R.id.vTFigureColor)
-    void chooseColorFifth() {
-        settingsPresenter.getEvent(R.id.vTFigureColor);
+    private fun chooseVerySlowSpeed() {
+        settingsPresenter?.getEvent(R.id.tvVerySlow)
     }
 
-    @OnClick(R.id.vJFigureColor)
-    void chooseColorSixth() {
-        settingsPresenter.getEvent(R.id.vJFigureColor);
+    private fun chooseSlowSpeed() {
+        settingsPresenter?.getEvent(R.id.tvSlow)
     }
 
-    @OnClick(R.id.tvVerySlow)
-    void chooseVerySlowSpeed() {
-        settingsPresenter.getEvent(R.id.tvVerySlow);
+    private fun chooseDefaultSpeed() {
+        settingsPresenter?.getEvent(R.id.tvDefault)
     }
 
-    @OnClick(R.id.tvSlow)
-    void chooseSlowSpeed() {
-        settingsPresenter.getEvent(R.id.tvSlow);
+    private fun chooseFastSpeed() {
+        settingsPresenter?.getEvent(R.id.tvFast)
     }
 
-    @OnClick(R.id.tvDefault)
-    void chooseDefaultSpeed() {
-        settingsPresenter.getEvent(R.id.tvDefault);
+    private fun chooseVeryFastSpeed() {
+        settingsPresenter?.getEvent(R.id.tvVeryFast)
     }
-
-    @OnClick(R.id.tvFast)
-    void chooseFastSpeed() {
-        settingsPresenter.getEvent(R.id.tvFast);
-    }
-
-    @OnClick(R.id.tvVeryFast)
-    void chooseVeryFastSpeed() {
-        settingsPresenter.getEvent(R.id.tvVeryFast);
-    }
-
 }
